2b6d77e6289d752451e646df6ab2ca1f073260b3
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2018-10-04 17:48:21 -0400

    Updated About page and made some footer links work

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ def why():
 def about():
     return render_template('about.html')
 
+# TO-DO:
+# Add templates and routing for Forms and Contact pages
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
