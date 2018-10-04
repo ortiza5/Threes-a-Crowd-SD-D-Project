1f6e7f8d98a2367263a30fa61ad90274c227e79e
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2018-10-04 17:37:59 -0400

    Total redesign to use Material Design Bootstrap

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 # Index
 @app.route('/')
 def index():
+    return render_template('startpage.html')
+
+@app.route('/home')
+def why():
     return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/why')
-def why():
-    return render_template('why.html')
 
 
 if __name__ == '__main__':
