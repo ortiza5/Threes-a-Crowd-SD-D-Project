cbff75d181bd91e34b3c866a28d170a442eef637
jingfei <zhouj5@rpil.edu>
2018-10-18 18:32:03 -0400

    added logging

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from config import *
 app = Flask(__name__)
 
 
+if __name__ != ‘__main__’:
+    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+
+
 # Index
 @app.route('/')
 def index():
