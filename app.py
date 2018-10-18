b7ca36d45158eb27c7737da951e94f255e5a059f
jingfei <zhouj5@rpil.edu>
2018-10-18 18:33:23 -0400

    fixed quote character

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from config import *
 app = Flask(__name__)
 
 
-if __name__ != ‘__main__’:
+if __name__ != '__main__':
     gunicorn_logger = logging.getLogger(‘gunicorn.error’)
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
