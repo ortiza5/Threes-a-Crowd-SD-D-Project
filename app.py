5e82cd447ce1a619b9777a65501f463c5b769835
jingfei <zhouj5@rpil.edu>
2018-10-18 17:15:48 -0400

    implemented basic login
    changed app.py and login.html
    made correct.html and wrong.html for testing

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask import Flask, render_template, flash, redirect, \
+                    url_for, session, request, logging
+import pymysql
+from passlib.hash import sha256_crypt
+from config import *
+
 
 app = Flask(__name__)
+
+
 # Index
 @app.route('/')
 def index():
@@ -21,13 +28,44 @@ def contact():
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+        # Open database connection
+        db = pymysql.connect(HOST,USER,PASSWORD,DBNAME )
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+        # execute SQL query using execute() method.
+        result = cursor.execute('SELECT * FROM user WHERE rcsid = %s', [username])
+
+        if result>0:    
+            # Fetch a single row using fetchone() method.
+            data = cursor.fetchone()
+            print('\n')
+            print(data)
+            print('\n')
+            password = data[2]
+            # Compare Passwords
+            print(password)
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                db.close()
+                print('correct')
+                return render_template('correct.html')
+            else:
+                db.close()
+                print('wrong pass')
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
         else:
-            return redirect('/home')
-    return render_template('login.html', error=error)
+            db.close()
+            print('wrong name')
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+        
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
@@ -47,4 +85,5 @@ def form1():
 
 
 if __name__ == '__main__':
+    app.secret_key=SECRET
     app.run(host="0.0.0.0", debug=True)
