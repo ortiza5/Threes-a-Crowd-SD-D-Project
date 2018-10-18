41f9da451f9f69369c994770f529f76eb0f20c29
jingfei <zhouj5@rpil.edu>
2018-10-18 18:34:31 -0400

    more quote character fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
