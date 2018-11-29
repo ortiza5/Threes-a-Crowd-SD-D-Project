467c44af29dc8efba48c860e7eacd833e88afdb3
Jingfei <zhouj5@rpi.edu>
2018-11-29 13:36:44 -0500

    fixed typo

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ def update_status(fid,owner,status):
 
 
 def send_email(subject, sender, recipients, text_body):
-    msg = Message(subject, sender=sender, recipients=precipients)
+    msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     # msg.html = html_body
     mail.send(msg)
