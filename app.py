b4ba1f8bff858fa319acadacb5f521fdd370d4d7
Jingfei <zhouj5@rpi.edu>
2018-11-28 21:34:38 -0500

    added sess.init_app back

--- app.py
+++ app.py
@@ -298,5 +298,5 @@ def delete_filledform(fid):
 
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
-    # sess.init_app(app)
+    sess.init_app(app)
     app.run(host="0.0.0.0", debug=True)
