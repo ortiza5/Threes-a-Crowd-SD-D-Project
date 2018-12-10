ef94108488332a6ece91081321d161793e570b6c
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2018-12-10 15:47:21 -0500

    All Python Files linted

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import app
 from config import *
 
 if __name__ == "__main__":
-	app.secret_key=SECRET
-	app.config['SESSION_TYPE'] = 'filesystem'
-	sess.init_app(app)
-	app.run()
+    app.secret_key = SECRET
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess.init_app(app)
+    app.run()
